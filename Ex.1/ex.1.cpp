7c5cd6f858e390114151d33a418aeaf193a438b1
gokulapriyacourse08-hub <gokulapriyacourse08@gmail.com>
2026-08-06 11:38:39 +0530

    Add files via upload

---
+++ Ex.1/ex.1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while (str[i] != '\0') {
+
+        if (isalpha(str[i]))
+            printf("%c : Identifier\n", str[i]);
+
+        else if (isdigit(str[i]))
+            printf("%c : Constant\n", str[i]);
+
+        else if (str[i] == '+' || str[i] == '-' || str[i] == '*' ||
+                 str[i] == '/' || str[i] == '=')
+            printf("%c : Operator\n", str[i]);
+
+        i++;
+    }
+
+    return 0;
+}
